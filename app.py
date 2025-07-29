99df81ed15a60517f5030cdd62bcb706716352c9
Nandhini Venkatesan <nandhinivenkatesan07@gmail.com>
2025-07-30 05:09:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from input_emotion import get_detected_intent
+from voice_generator import speak
+
+def main():
+    intent_text = get_detected_intent()
+    print("Detected Intent:", intent_text)
+    speak(intent_text)
+
+if __name__ == "__main__":
+    main()
 

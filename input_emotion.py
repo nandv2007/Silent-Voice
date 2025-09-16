66804fc357a07c40dda892ba02157215162814f3
Nandhini Venkatesan <nandhinivenkatesan07@gmail.com>
2025-09-16 14:37:00 +0530

    Update input_emotion.py

--- input_emotion.py
+++ input_emotion.py
@@ -1,13 +1,10 @@
-
-def get_detected_intent():
-    
+def get_detected_intent():    
     print("Simulating AI: Choose your intent")
     print("1. I need water")
     print("2. I am feeling pain")
     print("3. I want help")
     print("4. I am hungry")
     choice = input("Enter choice (1-4): ")
-
     mapping = {
         "1": "I need water",
         "2": "I am feeling pain",

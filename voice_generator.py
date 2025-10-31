eae72d8f19d497a9431ef459e3d2397119ff829a
Nandhini Venkatesan <nandhinivenkatesan07@gmail.com>
2025-10-31 06:03:51 +0530

    Delete voice_generator.py

--- voice_generator.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-def speak(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150) 
-    engine.setProperty('volume', 1) 
-    engine.say(text)
-    engine.runAndWait()
-
